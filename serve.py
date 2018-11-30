b190216afe1907eb2c29557b274070ad208f4d7c
Demian <dembanakh01@gmail.com>
2018-11-30 18:24:06 +0100

    Update serve.py

--- serve.py
+++ serve.py
@@ -22,7 +22,7 @@ server = smtplib.SMTP('smtp.gmail.com', 587)
 server.ehlo()
 server.starttls()
 server.ehlo()
-server.login("dembanakh01@gmail.com", "torres2001portu")
+server.login("dembanakh01@gmail.com", "********")
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
